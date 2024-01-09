9a3e0191e4d08734d4a59baf25075fa0e81b93bc
KamiyaGaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2024-01-09 04:38:43 +0000

    Feature: Add resume and github buttons on homepage

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,10 @@ export const Home = () => {
         <div className="content">
             <h4>Hi, I am Kamiya Gaikwad</h4>
             <h4>I am a Frontent Developer</h4>
+            <div className="buttons">
+            <a className="hmb" href={require("../assets/kamiya_gaikwad_web_development_resume.pdf")} download="kamiya_gaikwad_web_development_resume.pdf">Resume</a>
+            <a className="hmb" href="https://github.com/KamiyaGaikwad/" target="_blank">Github</a>
+            </div>
         </div>
       </div>
     )
