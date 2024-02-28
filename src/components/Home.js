2f9320a5a4cd14d066548a09ca9ad5795da6c8c7
Kamiya Gaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2024-02-28 14:23:04 +0000

    fixed spelling mistake in Home.js

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,7 +7,7 @@ export const Home = () => {
         <div className="image"><img src={pic} alt="cat"/></div>
         <div className="content">
             <h4>Hi, I am Kamiya Gaikwad</h4>
-            <h4>I am a Frontent Developer</h4>
+            <h4>I am a Frontend Developer</h4>
             <div className="buttons">
             <a className="hmb" href={require("../assets/kamiya_gaikwad_web_development_resume.pdf")} download="kamiya_gaikwad_web_development_resume.pdf">Resume</a>
             <a className="hmb" href="https://github.com/KamiyaGaikwad/" target="_blank">Github</a>
