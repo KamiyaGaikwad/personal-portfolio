457d9e3e2bdf196e32ec93d23cacb80edab51fa3
Kamiya Gaikwad <79163529+KamiyaGaikwad@users.noreply.github.com>
2024-07-21 22:34:39 +0530

    updated download file name

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,7 +9,7 @@ export const Home = () => {
             <h4>Hi, I am Kamiya Gaikwad</h4>
             <h4>I am a Frontend Developer</h4>
             <div className="buttons">
-            <a className="hmb" href={require("../assets/Kamiya_Resume_FrontEnd_Engineer.pdf")} download="kamiya_gaikwad_web_development_resume.pdf">Resume</a>
+            <a className="hmb" href={require("../assets/Kamiya_Resume_FrontEnd_Engineer.pdf")} download="kamiya_gaikwad_frontEnd_engineer_resume.pdf">Resume</a>
             <a className="hmb" href="https://github.com/KamiyaGaikwad/" target="_blank">Github</a>
             </div>
         </div>
